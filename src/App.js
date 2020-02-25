4438f786c60754e76d0f70900dc0645beb056696
Kevin Ramírez Reyes <kramirez@scio.local>
2020-02-24 18:04:43 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProjectAssumption from './Components/projectAssumption';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ProjectAssumption></ProjectAssumption>
     </div>
   );
 }
 
 export default App;
+/*
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>Junior Developer</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>Middle Developer</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>Senior Developer</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>Technical Lead</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>Engagement Manager</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>QA</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <select className="custom-select">
+            <option defaultValue>Project Duration (months)</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+        </div>
+      </div>
+*/
---
+++ src/Components/projectAssumption.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+class ProjectAssumption extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value1: Number,
+            value2: Number};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('JR ' + this.state.value1 + 
+        'MID ' + this.state.value2
+        );
+        event.preventDefault();
+      }
+
+
+    render() {
+        return (
+            <div className="container">
+            <form onSubmit={this.handleSubmit}>
+                <div className="row mt-4">
+                    <div className="col-md-4">
+                        <label>Jr Developer</label>
+                        <select className="custom-select" value={this.state.value1} onChange={this.handleChange}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                    <div className="col-md-4">
+                    <label>Mid Developer</label>
+                        <select className="custom-select" value={this.state.value2} onChange={this.handleChange}>
+                            <option  value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                </div>
+                <input type="submit" value="Submit" />
+                </form>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default ProjectAssumption;
